51706ae76ad3c3ae147aba04ef374094888b78cc
Shricharan K.S <62522322+shricharanks108@users.noreply.github.com>
2022-06-18 21:10:46 -0400

    Add Collisions + Refactor Some Rendering

--- sensor.js
+++ sensor.js
@@ -6,12 +6,43 @@ class Sensor {
         this.raySpread = Math.PI / 2;
 
         this.rays = [];
-        
+        this.readings = [];
     }
 
-    update() {
+    update(roadBorders) {
         this.#castRays();
+
+        this.readings = [];
+        for (let i = 0; i < this.rays.length; i++) {
+            this.readings.push(this.#getReading(this.rays[i], roadBorders));
+        }
     }
+
+    #getReading(ray, roadBorders) {
+        let touches = [];
+        
+        for (let i = 0; i < roadBorders.length; i++) {
+            const touch = getIntersection(
+                ray[0],
+                ray[1],
+                roadBorders[i][0],
+                roadBorders[i][1]
+            );
+
+            if (touch) {
+                touches.push(touch);
+            }
+        }
+
+        if (touches.length == 0) {
+            return null;
+        } else {
+            const offsets = touches.map(e => e.offset);
+            const min = Math.min(...offsets);
+            return touches.find(e => e.offset == min);
+        }
+    }
+        
 
     #castRays() {
         this.rays = [];
@@ -38,11 +69,23 @@ class Sensor {
 
     draw(ctx) {
         for (let i = 0; i < this.rayCount; i++) {
+            let end = this.rays[i][1];
+            if (this.readings[i]) {
+                end = this.readings[i];
+            }
+
             ctx.beginPath();
             ctx.lineWidth = 2;
             ctx.strokeStyle = "yellow";
             ctx.moveTo(this.rays[i][0].x, this.rays[i][0].y);
-            ctx.lineTo(this.rays[i][1].x, this.rays[i][1].y);
+            ctx.lineTo(end.x, end.y);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "black";
+            ctx.moveTo(this.rays[i][1].x, this.rays[i][1].y);
+            ctx.lineTo(end.x, end.y);
             ctx.stroke();
         }
     }
